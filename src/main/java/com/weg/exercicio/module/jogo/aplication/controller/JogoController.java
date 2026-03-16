d0df9dfa6d99e0b6432e44ae6626535cc142da50
LorhanPierre <lorhanpierre11102007@gmail.com>
2026-03-16 16:12:26 -0300

    feat: cria endpoint de post e get do modulo de jogos

--- src/main/java/com/weg/exercicio/module/jogo/aplication/controller/JogoController.java
+++ src/main/java/com/weg/exercicio/module/jogo/aplication/controller/JogoController.java
@@ -1,16 +1,69 @@
 package com.weg.exercicio.module.jogo.aplication.controller;
 
+import com.weg.exercicio.module.jogo.aplication.dto.JogoCreateRequestDto;
+import com.weg.exercicio.module.jogo.aplication.dto.JogoResponseDto;
 import com.weg.exercicio.module.jogo.domain.service.JogoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
 @RequestMapping("/jogo")
+@Tag(name = "Jogos", description = "Operações CRUD para gerenciamento de Jogos")
 public class JogoController {
 
     private final JogoService jogoService;
+
+    @PostMapping
+    @Operation(summary = "Cadastra um novo Jogo",
+            description = "Salva os dados de um novo jogo no banco de dados e devolve o registro criado"
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Jogo cadastrado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Dados inválidos na requisição"),
+            @ApiResponse(responseCode = "500", description = "Erro no código da API")
+    })
+    public ResponseEntity<JogoResponseDto> salvarJogo(
+            @Parameter @Valid @RequestBody JogoCreateRequestDto jogoRequest
+    ) {
+        try {
+            var novoJogo = jogoService.save(jogoRequest);
+
+            return ResponseEntity.status(HttpStatus.CREATED)
+                    .body(novoJogo);
+        } catch (RuntimeException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    @GetMapping
+    @Operation(
+            summary = "Lista todos os jogos",
+            description = "Lista todos os jogos cadastrados no banco de dados"
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Jogos listados com sucesso"),
+            @ApiResponse(responseCode = "500", description = "Erro interno na API")
+    })
+    public ResponseEntity<List<JogoResponseDto>> listarJogos() {
+        try {
+            var jogos = jogoService.findAll();
+
+            return ResponseEntity.ok(jogos);
+        } catch (RuntimeException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
 
 
 }
